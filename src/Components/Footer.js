2cfa095400cfd93875e4780bed31bdbb77ca2a48
meshacklee <meshacklee.com@gmail.com>
2025-01-03 16:20:56 +0100

    changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ const footer = () => {
           <h3><span className='Badge'>Address :</span> Kucukbakkalkoy mah. selv </h3>
           <h3>Lsk No. 4c Kapi No: 20 ATA EH R/ Istanbul</h3>
           <h4>+(90) 505 277 5435 (Turkiye)</h4>
-          <h4>admin@ecglobalpetrol.com</h4>
+          <h4>info@ecglobalpetrol.com</h4>
         </div>
 
 
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -10,7 +10,10 @@ const Profile = () => {
         <div class="card">
   <img src="./Asset/logooo.jpg" className="card-img-top" alt="..."/>
   <div class="card-body">
-    <p class="card-text">Mr. Eke Chika is the CEO managing director of EC Global oil Petrol</p>
+    <p class="card-text">Mr. Eke Chika is the CEO managing director of EC Global oil Petrol (Executive Chairman) He is a private investor with over 30years of investment experience in Africa and Eastern Europe with his University Accounting training he is exposed to a lot of people, organization and have a very good background on administration. He is the founder of EC GLOBAL OIL PETROL, a private investment company with investments across many sectors including infrastructural and resource sectors, mining and exploration and he is a co-founder and Director of the following companies Eksson concept limited, a private resource investment and Development Company.
+Having close associates  with companies like Mercury Engineering and Construction Company, Amco electrical company ltd and Joy petroleum ltd.
+He holds a BSc. Accounting From the University of Illorin and an M.B.A. in Finance. In addition, ready and willing to bring in a partners with the resources to support and limit the Company’s risk exposure. From where, Ecglobal oil and its partners will explore and develop the license areas of production and intrest.
+</p>
   </div>
 </div>
 
@@ -18,7 +21,7 @@ const Profile = () => {
 <div class="card">
   <img src="./Asset/2.jpg" className="card-img-top" alt="..."/>
   <div class="card-body">
-    <p class="card-text">DMr Gaga Unufe is one of the Directors of Ceneni Oil and Gas Company. He has a degree in Petroleum Engineering, and has working experience with Dadi Oil and Gas Company, where he worked for ﬁve years and Oso Marine and Energy where he worked for two years respectively. Apart from the oil and gas sector, he has managerial experience. He was one time a manager with Tiger Plastica Company owned 
+    <p class="card-text">Engr. Emeka Amadi is one of the Directors of Ec Global Oil and Gas Company. He has a degree in Petroleum Engineering, and has working experience with Dadi Oil and Gas Company, where he worked for ﬁve years and Oso Marine and Energy where he worked for two years respectively. Apart from the oil and gas sector, he has managerial experience. He was one time a manager with Tiger Plastica Company owned 
       by a Korean for a period of two years, and also operational manager with Tomahass Engineering Company for two years.</p>
   </div>
 </div>
@@ -29,10 +32,50 @@ const Profile = () => {
   <div class="card-body">
     <p class="card-text">ANITAESI
 DIRECTOR OF ADMIN
-A graduate of business administration and MBA in marketing, she has worked with many companies including Efe Finance Holdings, First Bank of Nigeria Plc and Tedway Nigeria Limited before moving over to Ceneni Oil & Gas Company Limted.</p>
+A graduate of business administration and MBA in marketing, she has worked with many companies including Efe Finance Holdings, First Bank of Nigeria Plc and Tedway Nigeria Limited before moving over to Ec Global petrol Oil & Gas Company Limted.</p>
   </div>
 </div>
 
+
+
+<div class="card">
+  <img src="./Asset/2666.jpg" className="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">ANITAESI
+DIRECTOR OF ADMIN
+A graduate of business administration and MBA in marketing, she has worked with many companies including Efe Finance Holdings, First Bank of Nigeria Plc and Tedway Nigeria Limited before moving over to Ec Global petrol Oil & Gas Company Limted.</p>
+  </div>
+</div>
+
+
+
+<div class="card">
+  <img src="./Asset/2666.jpg" className="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">BARR. AUSTIN AKAEGBU
+DIRECTOR LEGAL AFFAIRS.
+
+AUSTIN Akaegbu is the principal counsel at AUSTIN UCHE & Company. He is ﬁrst of all a Human Rights lawyer—whose aversion for injustice ﬁnds expression
+in all aspects of human Endeavour. His nous and knack in courtroom advocacy (Civil and Criminal Litigation) in many contested legal matters put him at the van of legal practice in Nigeria. He advises on all aspects of civil and criminal law in Nigeria and across multiple jurisdictions.
+He has particular expertise in:
+<ul>
+  <li>Corporate and Commercial Law</li>
+  <li>Human Rights Law</li>
+  <li>Aviation Law</li>
+  <li>Aviation Law</li>
+  <li>Insurance Law</li>
+  <li>Land Law</li>
+  <li>Immigration Law</li>
+  <li>Administrative Law</li>
+  <li>Admiralty and Maritime Law</li>
+  <li>Intellectual and Industrial Property Law</li>
+  <ol>etc</ol>
+</ul>
+
+AUSTIN advises lenders, borrowers, foreign investors and other third parties in the business world. He is retained by many commercial organizations. He also advises companies on their continuing obligations and corporate governance issues. He has severally worked and presently works with a team of lawyers and forensic ﬁnancial experts and fraud examiners who major in reconciliation, establishment and recovery of excess and illegal bank charges on loan/overdraft facilities and is currently in court over many of such cases.
+.</p>
+  </div>
+</div>
     </div>
 
 <Footer />
